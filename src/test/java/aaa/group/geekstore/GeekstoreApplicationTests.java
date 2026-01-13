0cd97ac8f6450a33914ca09a05ab4dab493a8b15
abubakr <dantecena@stud.fci-cu.edu.eg>
2026-01-13 17:59:20 +0200

    init

---
+++ src/test/java/aaa/group/geekstore/GeekstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package aaa.group.geekstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
